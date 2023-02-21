9e9240bb4bc868d00b0a3948a4e869e8133dc99f
mikaem <mkmanaligod@gmail.com>
2023-02-21 10:45:34 -0800

    yes

--- scripts/firebaseAPI-Team99.js
+++ scripts/firebaseAPI-Team99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyBR0vOcieqfl_RvRYP5rYnONYTV47MRxbQ",
+    authDomain: "demo0708-dadf1.firebaseapp.com",
+    projectId: "demo0708-dadf1",
+    storageBucket: "demo0708-dadf1.appspot.com",
+    messagingSenderId: "925370013020",
+    appId: "1:925370013020:web:c00038b34ea5e1b25bdd99"
 };
 
 //--------------------------------------------
